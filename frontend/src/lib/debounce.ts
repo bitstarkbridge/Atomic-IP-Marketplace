370ce7294de1f05a3deefc3b54c536f07119ecd7
dreamgene <dreamgene0855@gmail.com>
2026-03-29 13:57:10 +0100

    fix: add missing useDebounce hook (#587) (#600)

--- frontend/src/lib/debounce.ts
+++ frontend/src/lib/debounce.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Return a debounced copy of a changing value for lightweight UI filtering.
+ */
 export function useDebounce<T>(value: T, delay: number): T {
   const [debounced, setDebounced] = useState(value);
   useEffect(() => {
